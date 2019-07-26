c11ed156075bdab454fd22503fbb370649d03885
kneess <anibalcampos2324@gmail.com>
2019-07-26 11:59:32 -0700

    images ligned up

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var banner_height = $(".navbar").height();
+    var lastScrollTop = 0;
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      var currScrollTop = $(this).scrollTop();
+      if (scroll >= banner_height && currScrollTop > lastScrollTop) {
+        $(".navbar").hide();
+      } else {
+        $(".navbar").show();
+      }
+      lastScrollTop = currScrollTop;
+  
+    });
+  
+  });
